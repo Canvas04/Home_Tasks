75bc1cbdbb5c724f700ac516c020377478f38373
ruslanio_nwa <51386828+Canvas04@users.noreply.github.com>
2025-06-30 21:10:30 +0300

    Update UpgradableContracts.sol

--- UpgradableContracts.sol
+++ UpgradableContracts.sol
@@ -1,1 +1,56 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable@5.3.0/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable@5.3.0/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.3.0/proxy/utils/UUPSUpgradeable.sol";
+
+contract MyContractV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    string public text;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+
+    function setString(string calldata _newText) public {
+        text = _newText;
+    }
+}
+
+contract MyContractV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    string public text;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+
+    function setString(string memory _newText) public {
+        text = string.concat(text,_newText);
+    }
+
+}
